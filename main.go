1afc1c625e2df32ee751612f086241f328eb949c
Sarthak Neupane <sarthakrocks2004@gmail.com>
2024-12-23 23:20:41 -0600

    set up a basic server

--- main.go
+++ main.go
@@ -1,7 +1,37 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+	
+	"github.com/gin-gonic/gin"
+
+)
+
+type product struct {
+	Query string `json:"query"`
+	Type string `json:"type"`
+}
+
+var products = []product{}
+
+func handleQuery(context *gin.Context) {
+	var newProduct product
+	if err := context.BindJSON(&newProduct); err != nil {
+		fmt.Println(err)
+		context.IndentedJSON(http.StatusBadRequest, gin.H{
+			"message": "Invalid request",
+		})
+		return
+	}
+	products = append(products, newProduct)
+	context.IndentedJSON(http.StatusCreated, products)
+}
 
 func main() {
-	fmt.Println("Hello World")
+	// fmt.Println("Hello World")
+	router := gin.Default()
+	router.POST("/search", handleQuery)
+
+	router.Run("localhost:8080")
 }
